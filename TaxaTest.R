e4576158cc20a5a0b36fd6dfe144628937916e8a
Elizabeth Rudzki <Elr82@pitt.edu>
2019-12-05 13:35:43 -0500

    Cleaned up the script, added an export for the graph, fixed the export for the results, expanded on notation

--- TaxaTest.R
+++ TaxaTest.R
@@ -1,19 +1,29 @@
-#PLOTS-----------
-#Plotting AUC vs Observed OTUs
+#SETUP-----------
+#install.packages("lookupTable")
+library(lookupTable)
+#install.packages("plyr")
+library(plyr)
 #install.packages("ggplot2")
 library(ggplot2)
+#install.packages("magrittr")
+library(magrittr)
+
+data=read.csv("feature-table-6.csv")
+#Remove controls as log(0)=Inf
+datasub<-subset(data, auc_continuous!=0)
+taxakey=read.csv("Level6TaxaMetadata1.csv")
+
+#Plotting AUC vs Observed OTUs-----------
 
 #Scatter plot of relative abundance of Taxa952 in relation to the auc/infection severity
-data=read.csv("feature-table-6.csv")
-datasub<-subset(data, auc_continuous!=0)
-
 #install.packages("tidyverse")
 library(tidyverse)
 
+pdf('Taxa.pdf')
 ggplotRegression <- function(dat, xvar, yvar) {
   fml <- paste(yvar, "~", xvar)
   fit<-lm(fml,dat)
-  ggplot(data=readssub, aes(x=observed_otus,y=auc)) + geom_point() +
+  ggplot(data=datasub, aes(x=observed_otus,y=auc)) + geom_point() +
     scale_y_log10(breaks = trans_breaks("log10",function(x) 10^x),
                   labels = trans_format("log10", math_format(10^.x))) +
     geom_smooth(method="lm", se=TRUE, fullrange=FALSE, level=0.95, formula = y ~ x) +
@@ -23,24 +33,17 @@ ggplotRegression <- function(dat, xvar, yvar) {
                                            " P =",signif(summary(fit)$coef[2,4], 5))))
 }
 ggplotRegression(datasub, "auc", "observed_otus")
-
-#SETUP-----------
-#install.packages("lookupTable")
-library(lookupTable)
-#install.packages("plyr")
-library(plyr)
-
-data=read.csv("feature-table-6.csv")
-#Remove controls as log(0)=Inf
-datasub<-subset(data, auc_continuous!=0)
-taxakey=read.csv("Level6TaxaMetadata1.csv")
+dev.off()
 
 #FULL-DATASET---------------
 #Store the column names of all the taxa into a variable (-1-26) excluding metadata columns
 Taxa<-colnames(datasub[-c(1:26)])
-#Things tested as row names
 #Have to organize the row names by the order the ANOVA outputs, otherwise values will not match their name
 x<-data.frame(row.names= c("sex","line","batch","behav","TaxaN","seqbatch","sex:line"))
+
+#For every bacterial taxa, conduct a linearized model to determine whether the relative abundance of that specific taxa has
+#a significant affect on the log of the area under the curve (auc, infection severity), while taking into account
+#factors such as sex, line, batch, etc.
 for(i in Taxa){
   tempcolindex<-as.numeric(substr(i,5,nchar(i)))
   tempcolindex<- tempcolindex+26
@@ -50,54 +53,46 @@ for(i in Taxa){
   tempanova<-anova(templm)
   x<-cbind(x,tempanova$`Pr(>F)`[1:7])
 }
-#The NAs are in the sex:line row because the ANOVA moves the value for that up where the TaxaN was, when there were singularities in the Taxa rel abund
+
 colnames(x)<-Taxa
 
-#Number of taxa that should be removed in removedx
+#Number of taxa that should be removed in subsequent removedx df
 rowSums(is.na(x))
 
-#Filtering out the NA taxa should still result in removing the singularity-taxa.. since the NA result due to that?
+#Filtering out the NA taxa should still result in removing the singularity-taxa.. since the NA result due to that
+#The NAs are in the sex:line row because the ANOVA moves the value for that up where the TaxaN was
 removedx<-na.omit(t(x))
 removedx<-t(removedx)
-#write.table(removedx, "pvalR.txt", sep = "\t")
 
 #Num. sig taxa before correction
 uncorrected<-removedx["TaxaN",]<=0.05
 table(uncorrected)["TRUE"]
 #In this case, 102 significant prior to correction
 
-#Calculate Bonferroni p value by # of taxa left in removedx
-#a<-0.05/833
-
-#Subset and extract only row 5 (p values for the Taxa)
+#Subset and extract only row 5 (anova p values for the Taxa)
 removedxtaxa<-removedx[c(5), , drop=FALSE]
 taxacol<-colnames(removedxtaxa)
-library(magrittr)
-#ncol change
+#conduct FDR correction for multiple comparisons
+#change ncol based on removedxtaxa column num
 fdrcorrection<-(matrix(p.adjust(as.vector(as.matrix(removedxtaxa)), method='fdr'),ncol=833))
-#class(fdrcorrection)
-#class(subsetsigcol)
-#class(subsetsig)
+
 dfr<-as.data.frame(t(fdrcorrection))
 dtaxa<-as.list(t(taxacol))
 
 dfr$taxa<-dtaxa
-View(dfr)
 
-#Add Taxa by taxa name
+#Add Taxa by taxonomic name
 dfr$name<-taxakey$Name[match(dfr$taxa,taxakey$ID)]
 
-#How many taxa have a p value less than or equal to 0.05?
+#How many taxa have a p value less than or equal to 0.05 after FDR correction?
 tm<-dfr[,"V1"]<=0.05
 table(tm)["TRUE"]
 #print(tm)
 #Which taxa are those?
 tempsigdata<-which(dfr[,"V1"] <=0.05, arr.ind=TRUE)
-#list of row numbers where this is true
-#print(tempsigdata)
 #returns row numbers as well as Taxa numbers
 
-#Subset dfr by including only rows in tempsigdata
+#Subset dfr by including only rows in tempsigdata, export
 dfrsub<-dfr[tempsigdata, , drop=FALSE]
-
-write.table(dfr, "full-analysis-fdr-corrected.txt", sep="\t")
+dfrsub <- apply(dfrsub,2,as.character)
+write.csv(dfrsub, "full-analysis-fdr-corrected.csv")
